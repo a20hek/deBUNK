e04288f3fa5efbe746c3d936dd0c1772b7cdf917
abhishek <abbhishekk27@gmail.com>
2022-07-15 10:46:25 +0530

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: '100',
+  singleQuote: true,
+  semi:'true',
+  tabWidth:'2',
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+};
---
+++ App.tsx
@@ -0,0 +1,17 @@
+// /**
+//  @format
+//  */
+
+import React from 'react';
+import {Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+type Props = {}
+
+export default function App({}: Props) {
+  return (
+    <NavigationContainer>
+      <Text>App</Text>
+    </NavigationContainer>
+  );
+}
